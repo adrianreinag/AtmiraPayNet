dfd3db1b86af6fc4960712248ccf0489650893ef
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-05-10 14:07:24 +0200

    Creacion de las pantallas de inicio de sesion y registro y sis servicios

---
+++ AtmiraPayNet.Client/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using AtmiraPayNet.Client.Services.Interfaces;
+using AtmiraPayNet.Shared.DTO;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace AtmiraPayNet.Client.Services
+{
+    public class AuthService(HttpClient http) : IAuthService
+    {
+        private readonly HttpClient _http = http;
+
+        public async Task<string?> Register(RegisterDTO request)
+        {
+            var result = await _http.PostAsJsonAsync($"/api/auth/register", request);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<dynamic>(content);
+
+                return response!.token;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<string?> Login(LoginDTO request)
+        {
+            var result = await _http.PostAsJsonAsync($"/api/auth/login", request);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<dynamic>(content);
+
+                return response!.token;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ AtmiraPayNet.Client/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AtmiraPayNet.Shared.DTO;
+
+namespace AtmiraPayNet.Client.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string?> Register(RegisterDTO request);
+        Task<string?> Login(LoginDTO request);
+    }
+}
