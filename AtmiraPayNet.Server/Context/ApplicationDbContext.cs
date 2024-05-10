05ea2891bc4267148522e7fd18e19c5a21bd6b7e
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-05-10 09:31:50 +0200

    Creación del DbContext

---
+++ AtmiraPayNet.Server/Context/ApplicationDbContext.cs
@@ -0,0 +1,73 @@
+﻿using AtmiraPayNet.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmiraPayNet.Server.Context
+{
+    public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options), IApplicationDbContext
+    {
+        required public DbSet<Bank> Banks { get; set; }
+        required public DbSet<BankAccount> BankAccounts { get; set; }
+        required public DbSet<Payment> Payments { get; set; }
+        required public DbSet<PaymentLetter> PaymentLetters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Bank - BankAccount (1 - N)
+            modelBuilder.Entity<BankAccount>()
+                .HasOne(ba => ba.Bank)
+                .WithMany(b => b.BankAccounts)
+                .HasForeignKey(b => b.BankId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // BankAccount (source) - Payment (1 - N)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.SourceAccount)
+                .WithMany(ba => ba.SourcePayments)
+                .HasForeignKey(p => p.SourceAccountId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // BankAccount (destination) - Payment (1 - N)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.DestinationAccount)
+                .WithMany(ba => ba.DestinationPayments)
+                .HasForeignKey(p => p.DestinationAccountId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // BankAccount (interediary) - Payment (1 - N)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.IntermediaryAccount)
+                .WithMany(ba => ba.IntermediaryPayments)
+                .HasForeignKey(p => p.IntermediaryAccountId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // User - Payment (1 - N)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Payments)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // Payment - PaymentLetter (1 - 1)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.PaymentLetter)
+                .WithOne(pl => pl.Payment)
+                .HasForeignKey<PaymentLetter>(r => r.PaymentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Convert string to IBAN
+            modelBuilder.Entity<BankAccount>()
+                .Property(ba => ba.IBAN)
+                .HasConversion(
+                    v => v.Value,
+                    v => new IBAN(v)
+                );
+
+            // JSON Column Address
+            modelBuilder.Entity<Payment>()
+                .OwnsOne(p => p.Address, a => a.ToJson());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AtmiraPayNet.Server/Context/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AtmiraPayNet.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmiraPayNet.Server.Context
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<PaymentLetter> PaymentLetters { get; set; }
+    }
+}
